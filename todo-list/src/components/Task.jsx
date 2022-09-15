9f82beb502e3a0f80c59221118f148136ab76dcd
Maíra Costa <mairaccst@gmail.com>
2022-09-15 17:22:57 -0300

    add styling to the tasks and hover effects

--- todo-list/src/components/Task.jsx
+++ todo-list/src/components/Task.jsx
@@ -2,11 +2,16 @@ import styles from './Task.module.css'
 import { Trash } from "phosphor-react";
 
 export function Task({ content }) {
-    return (
-        <form className={styles.task}>
-            <input type="radio" id="task"/>
-            <label htmlFor="task">{content}</label>
-            <Trash size={24}/>
-        </form>
+    return (  
+        <div className={styles.wrapper}>
+            <label className={styles.task}>
+                <input type="checkbox"/>
+                <span className={styles.checkmark}></span>
+                <span className={styles.content}>{content}</span>
+            </label>
+            <button title='Deletar tarefa'>
+                <Trash size={20}/>
+            </button>
+        </div>
     )
 }
