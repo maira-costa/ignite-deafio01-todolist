07f6810dc2eec18d02bb0995b2e6963aa864ebd4
Maíra Costa <mairaccst@gmail.com>
2022-09-20 23:41:40 -0300

    add taks object and completed taks count

--- todo-list/src/components/Task.jsx
+++ todo-list/src/components/Task.jsx
@@ -1,26 +1,40 @@
 import styles from './Task.module.css'
 import { Trash } from "phosphor-react";
+import emptyIcon from '../assets/empty-icon.svg'
 
-export function Task({ content, checked, index, onCheckTask, onDeleteTask }) {
+export function Task({ onCompleteTask, content, onCheckTask, onDeleteTask, id, hasTasks }) {
 
     function handleDeleteTask() {
-        onDeleteTask(content)
+        onDeleteTask(id)
     }
 
     function handleCheckTask() {
-        onCheckTask(index)
+        onCheckTask(id, onCompleteTask)
     }
 
-    return (  
-        <div className={styles.wrapper}>
-            <label className={styles.task}>
-                <input type="checkbox" checked={checked[index]} onChange={handleCheckTask} />
-                <span className={styles.checkmark}></span>
-                <span className={styles.content}>{content}</span>
-            </label>
-            <button title='Deletar tarefa' onClick={handleDeleteTask}>
-                <Trash size={20}/>
-            </button>
-        </div>
-    )
+    if(hasTasks) {
+        return (  
+            <li className={styles.wrapper} id={id}>
+                <label className={styles.task}>
+                    <input type="checkbox" checked={onCompleteTask} onChange={handleCheckTask} />
+                    <span className={styles.checkmark}></span>
+                    <span className={styles.content}>{content}</span>
+                </label>
+                <button title='Deletar tarefa' onClick={handleDeleteTask}>
+                    <Trash size={20}/>
+                </button>
+            </li>
+        )
+    } else {
+        return (
+            <div className={styles.noTasks}>
+                <img src={emptyIcon} alt="" />
+                <p>
+                    <strong>Você ainda não tem tarefas cadastradas</strong>
+                    Crie tarefas e organize seus itens a fazer
+                </p>
+            </div> 
+        )
+    }
+    
 }
--- todo-list/src/components/Tasks.jsx
+++ todo-list/src/components/Tasks.jsx
@@ -1,52 +1,81 @@
 import { useState } from 'react'
-import emptyIcon from '../assets/empty-icon.svg'
+import { v4 as uuidv4 } from 'uuid'
 import { AddNewTask } from './AddNewTask'
 import { Task } from './Task'
 import styles from './Tasks.module.css'
 
+
 export function Tasks() {
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState([
+        // {
+        //     id: uuidv4(),
+        //     text: 'Comprar maçã',
+        //     isComplete: false
+        // },
+        // {
+        //     id: uuidv4(),
+        //     text: 'Comprar laranja',
+        //     isComplete: false
+        // }
+    ])
+    const [newTask, setNewTask] = useState({})
 
     const [newTaskText, setNewTaskText] = useState('')
-   
+
+    const [completedTasks, setCompletedTasks] = useState([])
+
     function handleCreateNewTask() {
         event.preventDefault()
-        setTasks([...tasks, newTaskText])
+    
+        setTasks([newTask, ...tasks])
+
         setNewTaskText('')
     }
 
     function handleNewTaskChange() {
         event.target.setCustomValidity('')
         setNewTaskText(event.target.value)
+        setNewTask({
+            id: uuidv4(),
+            text: newTaskText,
+            isComplete: false
+        })
+    }
+
+    function checkTask(checkedTaskId) {
+        const updatedCheckedState = tasks.map(task => {
+            if (task.id === checkedTaskId) {
+                return {...task, isComplete: !task.isComplete};
+            }
+            return task;
+        })
+           
+        setTasks(updatedCheckedState)   
+        
+        countCompletedTasks(updatedCheckedState)
+
     }
 
     function deleteTask(taskToDelete) {
         const tasksWithoutDeletedOne = tasks.filter(task => {
-            return task != taskToDelete
+            return task.id !== taskToDelete
         })
 
         setTasks(tasksWithoutDeletedOne)
-    }
 
-    const [checkedState, setCheckedState] = useState(
-        new Array(tasks.length).fill(false)
-    )
-
-    const [completedTasks, setCompletedTasks] = useState([])
-
-    function checkTask(position) {
-        console.log(checkedState)
-        const updatedCheckedState = checkedState.map((item, index) =>
-            index === position ? !item : item
-        )
-        setCheckedState(updatedCheckedState)
+        countCompletedTasks(tasksWithoutDeletedOne)
         
-        const checkedTasks = updatedCheckedState.filter(item => {
-            return item ===true
+    }
+    
+    function countCompletedTasks(updatedTask) {
+        const checkedTasks = updatedTask.filter(task => {
+            return task.isComplete === true
         })
-
+       
         setCompletedTasks(checkedTasks)
     }
+
+    const hasTasks = tasks.length > 0
 
     return (
         <div>
@@ -65,25 +94,21 @@ export function Tasks() {
                     <span>{completedTasks.length} de {tasks.length}</span>
                 </div>
             </div>
-            <div className={styles.noTasks}>
-                <img src={emptyIcon} alt="" />
-                <p>
-                    <strong>Você ainda não tem tarefas cadastradas</strong>
-                    Crie tarefas e organize seus itens a fazer
-                </p>
-            </div>
-            <div className={styles.hasTasks}>
-                {tasks.map((task, index) => {
-                    return <Task
-                                key={task}
-                                index={index}
-                                content={task}
-                                checked={checkedState}
+            <ul className={styles.tasksWrapper}>
+                {tasks.map(task => {
+                        return (
+                            <Task
+                                key={task.id}
+                                id={task.id}
+                                onCompleteTask={task.isComplete}
+                                content={task.text}
                                 onCheckTask={checkTask}
                                 onDeleteTask={deleteTask}
-                           />            
+                                hasTasks={hasTasks}
+                            />                           
+                        )
                 })}
-            </div>
+            </ul>
         </div>
     )
 }
